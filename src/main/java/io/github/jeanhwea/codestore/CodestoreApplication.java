24e6208d1dec4d783cd7b2bbd1d8951fd9c8ed9a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-03-26 23:05:55 +0800

    add

--- src/main/java/io/github/jeanhwea/codestore/CodestoreApplication.java
+++ src/main/java/io/github/jeanhwea/codestore/CodestoreApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodestoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodestoreApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CodestoreApplication.class, args);
+  }
 }
--- src/test/java/io/github/jeanhwea/codestore/CodestoreApplicationTests.java
+++ src/test/java/io/github/jeanhwea/codestore/CodestoreApplicationTests.java
@@ -1,13 +1,16 @@
 package io.github.jeanhwea.codestore;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CodestoreApplicationTests {
+  public static final Logger LOG = LoggerFactory.getLogger(CodestoreApplicationTests.class);
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {
+    LOG.info("context loads successful");
+  }
 }
